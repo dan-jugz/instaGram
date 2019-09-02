522c524f4a4700ee0d9035b1ede5d6a33419a49f
daniel njuguna <njugunadaniel364@gmail.com>
2019-09-02 10:37:18 +0300

    add the new profile forms  script that models a new profile image and form

---
+++ user_profile/forms.py
@@ -0,0 +1,13 @@
+from homepage.models import Profile, Image
+from django import forms
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user_id', 'bio', 'user_id', 'first_name', 'last_name', 'email', 'phone', 'mobile']
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['uploaded_by','comments', 'likes', 'liked']
+        
