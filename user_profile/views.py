5dccbeb3b6b357401107f99339e049a7158c0bd2
daniel njuguna <njugunadaniel364@gmail.com>
2019-09-02 10:53:15 +0300

    define a function that renders a new profile for the user

--- user_profile/views.py
+++ user_profile/views.py
@@ -23,3 +23,32 @@ def profile_page(request):
     print('+' * 30)
     print(user_images)
     return render(request, 'profile/profile_page.html', {'user_images':user_images, 'following':following.count()})
+
+@login_required(login_url='/accounts/login')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST'  :
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        bio = request.POST.get('bio')
+        print('*' * 30)
+        print(bio)
+        print('*' * 30)
+        print(first_name, last_name, phone, email)
+        user_id = current_user.id
+        if Profile.objects.filter(user_id=user_id):
+            user = Profile.objects.filter(user_id=user_id)
+           
+            form = NewProfileForm(request.POST, request.FILES)
+            if form.is_valid():
+                form = form.save()
+        
+            print('*' * 30)
+            # print(user)
+            user.update(first_name=first_name, last_name=last_name,phone=phone, email=email, bio=bio)
+        else: 
+            new_profile = Profile.objects.create(user_id=current_user.id, first_name=first_name, last_name=last_name,email=email,phone=phone, bio=bio)
+    return redirect(edit_profile_page)
+        
