10337c73ac4bbe24a18bced14689c566bf35e154
daniel njuguna <njugunadaniel364@gmail.com>
2019-09-02 15:08:06 +0300

    set all the required routes

--- instaGram/urls.py
+++ instaGram/urls.py
@@ -20,5 +20,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'',include('homepage.urls'))
+    url(r'',include('homepage.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/', views.logout, {"next_page": '/'}), 
+    url(r'^accounts/', include('user_profile.urls')),
 ]
