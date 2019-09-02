acbe8d82d8317a631e8ac30ba76e99c5fc017499
daniel njuguna <njugunadaniel364@gmail.com>
2019-09-02 08:22:34 +0300

    add the routes to signup and upload an image to the app

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_intro, name="intro" ),
     url('^suggestions/$', views.index, name='homepage'),
-
+    url(r'^signup/$', views.signup, name='signup'),   
+    url(r'^new/image$', views.new_image, name='new-image'),
 ]
